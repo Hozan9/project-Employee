df7eeddbe8e1b51d8f7d21fda546f7c79cb37717
Hozan9 <hozan.bahaden.93@gmail.com>
2023-04-27 18:38:10 +0200

    added Post and Delete in Backend

---
+++ project-Employee/backend/src/main/java/com/example/backend/model/Employee.java
@@ -0,0 +1,12 @@
+package com.example.backend.model;
+import org.springframework.data.annotation.Id;
+
+public record Employee(
+        @Id
+
+                 String id,
+                 String firstName,
+                 String lastName,
+                String email
+        ){}
+
---
+++ project-Employee/backend/src/main/java/com/example/backend/model/EmployeeController.java
@@ -0,0 +1,43 @@
+package com.example.backend.model;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class EmployeeController {
+
+    private  final EmployeeService employeeService;
+    private final  EmployeeRepository employeeRepository;
+
+    @GetMapping("/employees")
+    public ResponseEntity<List<Employee>> getAllEmployees(){
+    return ResponseEntity.ok(employeeService.findAll());
+}
+
+    @GetMapping("employees/{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable String id){
+
+        return ResponseEntity.ok(employeeService.findById(id));
+    }
+
+@PostMapping("/employees")
+public ResponseEntity<Employee> postEmployee(@RequestBody Employee employee) {
+    return new ResponseEntity<>(employeeService.saveEmployee(employee), HttpStatus.CREATED);
+}
+
+    @DeleteMapping("/employees/{id}")
+    public ResponseEntity<Object> deleteEmployee(@PathVariable String id) {
+        if (employeeRepository.existsById(id)) {
+            employeeService.deleteEmployee(id);
+            return ResponseEntity.noContent().build();
+        }
+        else return ResponseEntity.notFound().build();
+    }
+
+
+}
