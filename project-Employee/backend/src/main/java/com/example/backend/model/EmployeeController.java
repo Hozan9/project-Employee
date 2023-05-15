f93f5cfe1051713cd148bc49eb91b390b471bae7
Hozan9 <hozan.bahaden.93@gmail.com>
2023-05-15 12:21:09 +0200

    Add Image Upload

--- project-Employee/backend/src/main/java/com/example/backend/model/EmployeeController.java
+++ project-Employee/backend/src/main/java/com/example/backend/model/EmployeeController.java
@@ -4,8 +4,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.io.IOException;
 import java.util.List;
 @RestController
 @RequestMapping("/api/")
@@ -17,22 +19,30 @@ public class EmployeeController {
 
     @GetMapping("/employees")
     public ResponseEntity<List<Employee>> getAllEmployees() {
-
         return ResponseEntity.ok(employeeService.findAll());
     }
 
 
     @GetMapping("employees/{id}")
-
     public ResponseEntity<Employee> getEmployeeById(@PathVariable String id) {
-
         return ResponseEntity.ok(employeeService.findById(id));
     }
 
     @PostMapping("/employees")
-    public ResponseEntity<Employee> postEmployee(@RequestBody Employee employee) {
-        return new ResponseEntity<>(employeeService.saveEmployee(employee), HttpStatus.CREATED);
+    public ResponseEntity<Employee> postEmployee(@RequestPart("data")  Employee employee, @RequestPart(name="file",required = false) MultipartFile image) throws IOException {
+        return new ResponseEntity<>(employeeService.saveEmployee(employee,image), HttpStatus.CREATED);
     }
+
+   /* @PostMapping("/employees")
+    public ResponseEntity<Employee> postEmployee
+            (@RequestBody Employee employee) {
+        return new ResponseEntity<>(employeeService.
+                saveEmployee(employee), HttpStatus.CREATED);
+    }*/
+
+
+
+
     @PutMapping(path = {"/employees/{id}"})
     Employee update(@PathVariable String id,@RequestBody Employee employee){
         if(!employee.id().equals(id)) {
@@ -40,6 +50,8 @@ public class EmployeeController {
        }
         return  employeeService.update(employee);
     }
+
+
 
     @DeleteMapping("/employees/{id}")
     public ResponseEntity<Object> deleteEmployee(@PathVariable String id) {
--- project-Employee/frontend/src/LoginPage.tsx
+++ project-Employee/frontend/src/LoginPage.tsx
@@ -1,25 +1,61 @@
-import {FormEvent, useState} from "react";
+import {ChangeEvent, FormEvent, useState} from "react";
 import {useNavigate} from "react-router-dom";
+import "./LoginPage.css"
 type Props = {
-    onLogin: (username: string, password: string) => Promise<void>
+    onLogin: (username: string, password: string, image: File | undefined,
+              url: string) => Promise<void>
 }
 export default function LoginPage(props: Props){
     const [username,setUsername] = useState<string>("")
     const [password,setPassword] = useState<string>("")
     const navigate = useNavigate()
+    const [image,setImage] = useState<File>();
+
+   const [url,setUrl] = useState<string>( "");
+
     function onSubmit(event: FormEvent<HTMLFormElement>) {
         event.preventDefault()
 
-        props.onLogin(username, password)
+        props.onLogin(username, password,image,url)
             .then(() => {navigate("/employees")})
     }
+    function onFileChange(event: ChangeEvent<HTMLInputElement>) {
+        if (event.target.files) {
+            setImage(event.target.files[0])
+        }
+    }
+
+
      return(
-        <form onSubmit={onSubmit}>
-            <input value={username} placeholder="username" type="text" onChange={e => setUsername(e.target.value)}/>
+             <form onSubmit={onSubmit} action={"action_page.html"}
+                   method={"post"}>
 
-            <input value={password} placeholder="password" type="password" onChange={e => setPassword(e.target.value)}/>
-        <button type="submit">login</button>
-        </form>
+             <div className="container">
+                 <div className="imagecontainer">
+                     <input className={"postImage"} type={"file"}
+                            name="postImg" onChange={onFileChange}/>
 
+                 </div>
+                 <p></p>
+                 <img src={image ? URL.createObjectURL(image): ""}
+                      alt="Avatar"  className="avatar" width={"10"}/>
+
+
+                 <p className={"username"}>Username:</p>
+                 <input className={"input"} value={username}
+                        placeholder="Enter username" type="text"
+                        onChange={e => setUsername(e.target.value)}/>
+
+
+                 <p className={"password"}>Password:</p>
+                 <input className={"input2"} value={password}
+                        placeholder="Enter password" type="password"
+                        onChange={e => setPassword(e.target.value)}/>
+
+
+
+                 <button className={"login"} type="submit">login</button>
+             </div>
+            </form>
     );
 }
