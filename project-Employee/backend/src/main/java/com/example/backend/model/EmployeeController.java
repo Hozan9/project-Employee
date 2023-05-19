e1c57a0e1a251d402e6787f579cb0f6dee80407e
Hozan9 <hozan.bahaden.93@gmail.com>
2023-05-19 13:20:02 +0200

    Add UnitTest

--- project-Employee/backend/src/main/java/com/example/backend/model/EmployeeController.java
+++ project-Employee/backend/src/main/java/com/example/backend/model/EmployeeController.java
@@ -21,28 +21,14 @@ public class EmployeeController {
     public ResponseEntity<List<Employee>> getAllEmployees() {
         return ResponseEntity.ok(employeeService.findAll());
     }
-
-
     @GetMapping("employees/{id}")
     public ResponseEntity<Employee> getEmployeeById(@PathVariable String id) {
         return ResponseEntity.ok(employeeService.findById(id));
     }
-
     @PostMapping("/employees")
     public ResponseEntity<Employee> postEmployee(@RequestPart("data")  Employee employee, @RequestPart(name="file",required = false) MultipartFile image) throws IOException {
         return new ResponseEntity<>(employeeService.saveEmployee(employee,image), HttpStatus.CREATED);
     }
-
-   /* @PostMapping("/employees")
-    public ResponseEntity<Employee> postEmployee
-            (@RequestBody Employee employee) {
-        return new ResponseEntity<>(employeeService.
-                saveEmployee(employee), HttpStatus.CREATED);
-    }*/
-
-
-
-
     @PutMapping(path = {"/employees/{id}"})
     Employee update(@PathVariable String id,@RequestBody Employee employee){
         if(!employee.id().equals(id)) {
@@ -50,14 +36,9 @@ public class EmployeeController {
        }
         return  employeeService.update(employee);
     }
-
-
-
     @DeleteMapping("/employees/{id}")
-    public ResponseEntity<Object> deleteEmployee(@PathVariable String id) {
-        if (employeeRepository.existsById(id)) {
-            employeeService.deleteEmployee(id);
-            return ResponseEntity.noContent().build();
-        } else return ResponseEntity.notFound().build();
+    void deleteEmployee(@PathVariable String id) {
+        employeeService.deleteEmployee(id);
     }
+
 }
